e6814006bda59af735b1067c7599b1b881e0a279
ukrWily <ukrwily@gmail.com>
2024-08-25 13:27:45 +0300

    L course not found

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,16 +1,27 @@
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/courses";
+import NotFound from "./NotFound";
+import { useEffect } from "react";
 
 function SingleCourse() {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.courseSlug);
+
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" });
+    }
+  }, [course]);
 
   return (
     <>
-      <h1>Single Course Info</h1>
-      <h2>{course.title}</h2>
-      <h2>{course.slug}</h2>
-      <h2>{course.id}</h2>
+      <h1>{course?.title}</h1>
+      <h2>{course?.slug}</h2>
+      <h2>{course?.id}</h2>
+      <Link to=".." relative="path">
+        All courses
+      </Link>
     </>
   );
 }
